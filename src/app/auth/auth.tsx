206e04123825c55849addb818969a74420269b7c
Michelle Li <michelleyli123@gmail.com>
2023-11-14 16:23:44 -0500

    additional auth functions

--- src/app/auth/auth.tsx
+++ src/app/auth/auth.tsx
@@ -10,12 +10,27 @@ export const signIn = async () => {
     const token = credential?.accessToken;
     const user = result.user;
 
-    return { token, user }
+    return { token, user };
   } catch (err: any) {
     const code = err.code;
     const message = err.message;
     const email = err.customData.email;
 
-    console.log(`An error ${code} occurred when logging user with email: ${email} with message: ${message}`)
+    console.log(
+      `An error ${code} occurred when logging user with email: ${email} with message: ${message}`,
+    );
+    return null;
   }
+};
+
+export const getUserEmail = async () => {
+  if (!auth.currentUser) {
+    await signIn();
+  } else {
+    return auth.currentUser.email;
+  }
+};
+
+export const signOut = async () => {
+  await auth.signOut();
 };
