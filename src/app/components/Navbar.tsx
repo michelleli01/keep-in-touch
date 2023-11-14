52055262cd9612bf3962ae0604e6eb0c09c78c8d
Michelle Li <michelleyli123@gmail.com>
2023-11-14 16:32:58 -0500

    dynamically change login/sign out button

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,13 +1,28 @@
 "use client";
 
 import React, { useState } from "react";
-import {signIn} from "../auth/auth"
+import { signIn, signOut } from "../auth/auth";
 
 export default function Navbar() {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
+
+  const handleClick = async () => {
+    if(isLoggedIn){
+      await signOut();
+    } else {
+      await signIn();
+    }
+
+    setIsLoggedIn(!isLoggedIn)
+  }
+
   return (
     <div>
-      <button onClick={signIn}>Login</button>
+      {isLoggedIn ? (
+        <button onClick={handleClick}>Sign Out</button>
+      ) : (
+        <button onClick={handleClick}>Login</button>
+      )}
     </div>
   );
 }
